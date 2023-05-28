3d88eb1252f87d087dc473b948fc9acb97ff0b40
gafrielAR <muhammad.gafriel@gmail.com>
2023-05-28 15:46:50 +0700

    add template and change dashboard view

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,44 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="{{ route('admin.dashboard') }}">
+                <i class="ci-1 ci-dashboard-line"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="users-profile.html">
+                <i class="ci-1 ci-user-group"></i>
+                <span>Anggota</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-faq.html">
+                <i class="ci-1 ci-folder-2"></i>
+                <span>Simpanan</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-contact.html">
+                <i class="ci-1 ci-money-send"></i>
+                <span>Pinjaman</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-register.html">
+                <i class="ci-1 ci-money"></i>
+                <span>Angsuran</span>
+            </a>
+        </li>
+
+    </ul>
+
+</aside>
